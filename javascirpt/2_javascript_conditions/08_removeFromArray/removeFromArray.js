913bb2e316455dedc6cb56939904ee4c411e75aa
mors-templar <raybaalb@gmail.com>
2025-07-19 15:26:46 +0500

    8th excersise done

--- javascirpt/2_javascript_conditions/08_removeFromArray/removeFromArray.js
+++ javascirpt/2_javascript_conditions/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,6 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr , ...num ) {
+    //return arr.filter(element => element !== num)
+    return arr.filter(element => !num.includes(element));
 };
 
 // Do not edit below this line
