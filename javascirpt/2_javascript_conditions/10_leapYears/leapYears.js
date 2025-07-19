ab23483e333675cab8472484594f59541b17f096
mors-templar <raybaalb@gmail.com>
2025-07-19 16:11:09 +0500

    11th excersise done

--- javascirpt/2_javascript_conditions/10_leapYears/leapYears.js
+++ javascirpt/2_javascript_conditions/10_leapYears/leapYears.js
@@ -1,5 +1,13 @@
 const leapYears = function() {
-
+    let isYearDivisibleByFour = year % 4 === 0;
+    let isCentury = year % 100 === 0;
+    let isYearDivisibleByFourHundred = year % 400 === 0;
+    
+    if (
+    isYearDivisibleByFour &&
+    (!isCentury || isYearDivisibleByFourHundred))
+    {return true;} 
+    else {return false;}
 };
 
 // Do not edit below this line
