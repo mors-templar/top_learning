92b59a7b32e927576e65c79eef8f00ef3ad1bf20
mors-templar <raybaalb@gmail.com>
2025-07-19 14:46:35 +0500

    did javascript excersise 06

--- javascirpt/2_javascript_conditions/06_repeatString/repeatString.js
+++ javascirpt/2_javascript_conditions/06_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(strTOrepeat , numTOrepeat) {
+    if (numTOrepeat < 0) return "ERROR";
+    let outstr = "";
+    for(let x = 0;x < numTOrepeat;x++){
+        outstr +=strTOrepeat;
+    }
+    return outstr;
 };
 
 // Do not edit below this line
