6f8d0f4431a21b3a06e190a8f3029dfa6b01034f
mors-templar <raybaalb@gmail.com>
2025-07-21 14:43:50 +0500

    added a hover feature to the squares

--- javascirpt/etch-a-sketch/index.js
+++ javascirpt/etch-a-sketch/index.js
@@ -8,8 +8,23 @@ for (let row = 0; row < gridSize; row++) {
     for (let col = 0; col < gridSize; col++) {
         const square = document.createElement("div");
         square.classList.add("square");
+
+        // Add hover event listener directly to each square
+        square.addEventListener("mouseover", function() {
+            square_hover(square);
+        });
+        square.addEventListener("mouseout", function() {
+            square_out(square);
+        });
         rowDiv.appendChild(square);
     }
 
     container.appendChild(rowDiv);
+}
+
+function square_hover(element) {
+    element.style.borderColor = "brown";
+}
+function square_out(element){
+    element.style.borderColor = "white"
 }
