211a58eaf72a67c5ab52dcdd42064414d1865215
mors-templar <raybaalb@gmail.com>
2025-07-22 15:57:25 +0500

    solved all excersises

--- javascirpt/2_javascript_excersises/12_calculator/calculator.js
+++ javascirpt/2_javascript_excersises/12_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(a) {
+	a.reduce((total, current) => total + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(a) {
+  return a.reduce((prod,curr) => total * curr);
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return Math.pow(a,b);
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  if (a < 0) {
+    return undefined; 
+  } else if (a === 0 || a === 1) {
+    return 1;
+  } else {
+    let fact = 1;
+    for (let i = a; i > 1; i--) {
+      fact = fact * i;
+    }
+    return fact;
+  }
 };
 
 // Do not edit below this line
